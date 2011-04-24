1c9b279c3cf7c7487b15ec0c251ccb81b7d2d331
Phosfour <phosfour4@gmail.com>
2011-04-24 14:36:38 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Phosfour::Application.config.secret_token = 'ce16b1c5b6395486883bf2a6aec10f8c783791abb4b60c72ff261cc669bce2717794013f41f67710ab0a8a46e2581fff930000d4a8ddd54dc52d154212dd4321'
